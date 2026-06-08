b7acf103076ed1184903279faba405baf6f15c9b
frankgraya <francisco.graya26@gmail.com>
2026-06-08 15:00:51 -0600

    primer configuracion de archivos 1

---
+++ proyecto-crud/src/test/java/com/crud/proyecto_crud/ProyectoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.proyecto_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
